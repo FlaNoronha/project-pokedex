9805442c4caddd6b72550de49be79f1c1b2e1b2f
Flávia Noronha <116315293+FlaNoronha@users.noreply.github.com>
2023-05-14 02:07:36 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+alert("Welcome to the Pokédex Project of Dev em Dobro's 2023 course.");
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+  }
+});
